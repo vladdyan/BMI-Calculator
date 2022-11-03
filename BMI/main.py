81f5ca4983fd7466011d3ad1d99f19bafb692f70
Vlad <41800991+vladdyan@users.noreply.github.com>
2022-11-03 16:08:04 +0600

    New fith func

--- BMI/main.py
+++ BMI/main.py
@@ -1,23 +1,3 @@
-try:
-    weight = float(input('Enter your weight: '))
-    height = float(input('Enter your height: '))
-    BMI = weight / (height/100)**2
-    while True:
-        if BMI <= 0:
-            print('Please enter only positive numbers')
-            break
-        elif BMI < 18.5:
-            print('You have Underweight')
-            break
-        elif BMI <= 25:
-            print('You have Normal weight')
-            break
-        elif BMI <= 30:
-            print('You have Overweight')
-            break
-        elif BMI > 30:
-            print('You have Obesity')
-            break
-except:
-    print('Please enter only positive numbers')
+from function import main_code
 
+main_code()
