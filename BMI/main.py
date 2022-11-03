a6132298c5c63d69c77e0743b8bfa49b2d2c2933
Vlad <41800991+vladdyan@users.noreply.github.com>
2022-11-03 13:35:25 +0600

    test test
    test test

--- BMI/main.py
+++ BMI/main.py
@@ -1,2 +1,23 @@
+try:
+    weight = float(input('Enter your weight: '))
+    height = float(input('Enter your height: '))
+    BMI = weight / (height/100)**2
+    while True:
+        if BMI <= 0:
+            print('Please enter only positive numbers')
+            if BMI <= 0:break
+        elif BMI < 18.5:
+            print('You have Underweight')
+            if BMI < 18.5:break
+        elif BMI <= 25:
+            print('You have Normal weight')
+            if BMI <= 25:break
+        elif BMI <= 30:
+            print('You have Overweight')
+            if BMI <= 30:break
+        elif BMI > 30:
+            print('You have Obesity')
+            if BMI > 30:break
+except:
+    print('Please enter only positive numbers')
 
-1
